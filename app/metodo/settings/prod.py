813630f6ccf31d73513a7ed957ab5b2ba42d0021
msotomx <msoto.stop@gmail.com>
2025-04-01 14:41:27 -0600

    antes de generar docker-machine

--- app/metodo/settings/prod.py
+++ app/metodo/settings/prod.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # se agrego despues
 ]
 
 ROOT_URLCONF = 'metodo.urls'
@@ -124,12 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static'),
+#]
 
 # Para producción (cuando uses collectstatic)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = '/usr/src/app/metodo/staticfiles'
 
 
